f7c13f0cd6a81bdc74ea16c888a39924e31005b4
Gorla Penchala Narasimha <114098475+Narasimha191031018@users.noreply.github.com>
2026-07-07 18:28:40 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,20 @@
+output "resource_group_name" {
+  description = "The name of the resource group"
+  value       = azurerm_resource_group.demo.name
+}
+
+output "storage_account_name" {
+  description = "The name of the storage account"
+  value       = azurerm_storage_account.demo.name
+}
+
+output "prometheus_url" {
+  description = "Public IP/URL for Prometheus"
+  value       = azurerm_container_group.prometheus.ip_address[0].ip
+}
+
+output "grafana_url" {
+  description = "Public IP/URL for Grafana"
+  value       = azurerm_container_group.grafana.ip_address[0].ip
+}
 
