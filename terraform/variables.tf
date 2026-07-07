d62fe17cbaf71233b71faa2ded68ae6a1c570d1e
Gorla Penchala Narasimha <114098475+Narasimha191031018@users.noreply.github.com>
2026-07-07 18:28:05 +0530

    Define Terraform variables for Azure resources
    Added variables for location, resource group, storage account, and container.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,24 @@
+variable "location" {
+  description = "Azure region where resources will be created"
+  type        = string
+  default     = "East US"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "rg-checkov-demo"
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+  default     = "checkovdemostorage"
+}
+
+variable "container_name" {
+  description = "Name of the storage container for tfstate"
+  type        = string
+  default     = "tfstate"
+}
 
