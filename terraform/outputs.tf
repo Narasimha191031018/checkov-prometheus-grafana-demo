67ec2732682c360efdf0f8a6e3985b19231261c0
Gorla Penchala Narasimha <114098475+Narasimha191031018@users.noreply.github.com>
2026-07-08 12:33:16 +0530

    Refactor outputs for storage and monitoring services

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,19 @@
+output "storage_account_name" {
+  description = "Name of the storage account"
+  value       = azurerm_storage_account.demo.name
+}
+
+output "storage_container_name" {
+  description = "Name of the storage container"
+  value       = azurerm_storage_container.tfstate.name
+}
+
 output "prometheus_url" {
-  description = "Prometheus public URL"
-  value       = "http://${azurerm_container_group.prometheus.ip_address}:9090"
+  description = "Prometheus endpoint URL"
+  value       = "http://${azurerm_container_group.prometheus.container[0].name}.${var.location}.azurecontainer.io:9090"
 }
 
 output "grafana_url" {
-  description = "Grafana public URL"
-  value       = "http://${azurerm_container_group.grafana.ip_address}:3000"
-}
-
-output "resource_group_name" {
-  description = "The name of the resource group"
-  value       = azurerm_resource_group.demo.name
-}
-
-output "storage_account_name" {
-  description = "The name of the storage account"
-  value       = azurerm_storage_account.demo.name
+  description = "Grafana endpoint URL"
+  value       = "http://${azurerm_container_group.grafana.container[0].name}.${var.location}.azurecontainer.io:3000"
 }
