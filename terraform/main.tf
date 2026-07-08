1b1a60d9e6c3c49fd8405ec7ee3bed98000fef9a
Gorla Penchala Narasimha <114098475+Narasimha191031018@users.noreply.github.com>
2026-07-08 13:58:51 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,72 +12,39 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "demo" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "rg-checkov-demo"
+  location = "East US"
 }
 
 resource "azurerm_virtual_network" "demo" {
-  name                = var.vnet_name
-  address_space       = [var.vnet_address_space]
+  name                = "vnet-checkov-demo"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.demo.location
   resource_group_name = azurerm_resource_group.demo.name
 }
 
 resource "azurerm_subnet" "demo" {
-  name                 = var.subnet_name
+  name                 = "subnet-checkov-demo"
   resource_group_name  = azurerm_resource_group.demo.name
   virtual_network_name = azurerm_virtual_network.demo.name
-  address_prefixes     = [var.subnet_prefix]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_storage_account" "demo" {
-  name                     = var.storage_account_name
+  name                     = "checkovdemostorage"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
   min_tls_version          = "TLS1_2"
   allow_nested_items_to_be_public = false
   public_network_access_enabled   = false
-  shared_access_key_enabled       = false   # disable shared key auth
-
-  blob_properties {
-    delete_retention_policy {
-      days    = 7
-      #enabled = true   # enable soft delete
-    }
-  }
-
-  queue_properties {
-    logging {
-      delete                = true
-      read                  = true
-      write                 = true
-      version               = "1.0"
-      retention_policy_days = 7
-    }
-  }
 }
 
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
   storage_account_name  = azurerm_storage_account.demo.name
   container_access_type = "private"
-}
-
-# Private endpoint for storage account
-resource "azurerm_private_endpoint" "storage_pe" {
-  name                = "pe-checkov-storage"
-  location            = azurerm_resource_group.demo.location
-  resource_group_name = azurerm_resource_group.demo.name
-  subnet_id           = azurerm_subnet.demo.id
-
-  private_service_connection {
-    name                           = "psc-checkov-storage"
-    private_connection_resource_id = azurerm_storage_account.demo.id
-    subresource_names              = ["blob"]
-    is_manual_connection           = false
-  }
 }
 
 resource "azurerm_container_group" "prometheus" {
@@ -98,7 +65,7 @@ resource "azurerm_container_group" "prometheus" {
     }
   }
 
-  ip_address_type = "Private"   # no public IP
+  ip_address_type = "Private"
   subnet_ids      = [azurerm_subnet.demo.id]
 }
 
@@ -120,6 +87,6 @@ resource "azurerm_container_group" "grafana" {
     }
   }
 
-  ip_address_type = "Private"   # no public IP
+  ip_address_type = "Private"
   subnet_ids      = [azurerm_subnet.demo.id]
 }
