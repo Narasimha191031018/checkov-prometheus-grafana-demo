f04c350928882006e25ad39daab7537cd6e63280
Gorla Penchala Narasimha <114098475+Narasimha191031018@users.noreply.github.com>
2026-07-08 11:13:04 +0530

    Add Prometheus and Grafana URL outputs
    Added outputs for Prometheus and Grafana public URLs while removing IP outputs.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,13 @@
+output "prometheus_url" {
+  description = "Prometheus public URL"
+  value       = "http://${azurerm_container_group.prometheus.ip_address}:9090"
+}
+
+output "grafana_url" {
+  description = "Grafana public URL"
+  value       = "http://${azurerm_container_group.grafana.ip_address}:3000"
+}
+
 output "resource_group_name" {
   description = "The name of the resource group"
   value       = azurerm_resource_group.demo.name
@@ -6,14 +16,4 @@ output "resource_group_name" {
 output "storage_account_name" {
   description = "The name of the storage account"
   value       = azurerm_storage_account.demo.name
-}
-
-output "prometheus_ip" {
-  description = "Public IP for Prometheus"
-  value       = azurerm_container_group.prometheus.ip_address
-}
-
-output "grafana_ip" {
-  description = "Public IP for Grafana"
-  value       = azurerm_container_group.grafana.ip_address
 }
