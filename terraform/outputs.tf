4b78d4a5e5f57de0e6352972ddb953a11db8a238
Gorla Penchala Narasimha <114098475+Narasimha191031018@users.noreply.github.com>
2026-07-08 17:36:29 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,9 @@
-output "storage_account_name" {
-  description = "Name of the storage account"
-  value       = azurerm_storage_account.demo.name
-}
-
-output "storage_container_name" {
-  description = "Name of the storage container"
-  value       = azurerm_storage_container.tfstate.name
-}
-
 output "prometheus_url" {
-  description = "Prometheus endpoint URL"
-  value       = "http://${azurerm_container_group.prometheus.container[0].name}.${var.location}.azurecontainer.io:9090"
+  description = "Public URL for Prometheus"
+  value       = "http://${azurerm_container_group.prometheus.ip_address[0].fqdn}:9090"
 }
 
 output "grafana_url" {
-  description = "Grafana endpoint URL"
-  value       = "http://${azurerm_container_group.grafana.container[0].name}.${var.location}.azurecontainer.io:3000"
+  description = "Public URL for Grafana"
+  value       = "http://${azurerm_container_group.grafana.ip_address[0].fqdn}:3000"
 }
