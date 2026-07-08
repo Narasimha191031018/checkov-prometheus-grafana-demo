befc35abc2874be46313c1ad06a11a3af53c6302
Gorla Penchala Narasimha <114098475+Narasimha191031018@users.noreply.github.com>
2026-07-08 18:14:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,7 @@ resource "azurerm_container_group" "prometheus" {
 
   container {
     name   = "prometheus"
-    image  = "mcr.microsoft.com/oss/prometheus/prometheus"
+    image = "mcr.microsoft.com/oss/prometheus/prometheus:latest"
     cpu    = var.prometheus_cpu
     memory = var.prometheus_memory
 
@@ -57,7 +57,7 @@ resource "azurerm_container_group" "grafana" {
 
   container {
     name   = "grafana"
-    image  = "mcr.microsoft.com/oss/grafana/grafana"
+    image = "mcr.microsoft.com/oss/grafana/grafana:latest"
     cpu    = var.grafana_cpu
     memory = var.grafana_memory
 
