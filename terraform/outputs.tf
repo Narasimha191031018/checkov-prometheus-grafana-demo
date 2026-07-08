6f0c4b3b0ea22aa31286ba16d8422ff9a888cc88
Gorla Penchala Narasimha <114098475+Narasimha191031018@users.noreply.github.com>
2026-07-08 17:44:18 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "prometheus_url" {
   description = "Public URL for Prometheus"
-  value       = "http://${azurerm_container_group.prometheus.ip_address[0].fqdn}:9090"
+  value       = "http://${azurerm_container_group.prometheus.fqdn}:9090"
 }
 
 output "grafana_url" {
   description = "Public URL for Grafana"
-  value       = "http://${azurerm_container_group.grafana.ip_address[0].fqdn}:3000"
+  value       = "http://${azurerm_container_group.grafana.fqdn}:3000"
 }
