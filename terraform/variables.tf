e5f1c96b8a6281e1c9dbd3b5786bd69f9a8947b4
Gorla Penchala Narasimha <114098475+Narasimha191031018@users.noreply.github.com>
2026-07-08 12:33:55 +0530

    Refactor variables and add VNet configuration
    Refactored variables in variables.tf for clarity and added new variables for VNet and subnet.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,41 @@
-variable "location" {
-  description = "Azure region where resources will be created"
-  type        = string
-  default     = "East US"
-}
-
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
   default     = "rg-checkov-demo"
 }
 
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
+
+variable "vnet_name" {
+  description = "Virtual network name"
+  type        = string
+  default     = "vnet-checkov-demo"
+}
+
+variable "vnet_address_space" {
+  description = "Address space for the VNet"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_name" {
+  description = "Subnet name"
+  type        = string
+  default     = "subnet-checkov-demo"
+}
+
+variable "subnet_prefix" {
+  description = "Subnet address prefix"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
 variable "storage_account_name" {
-  description = "Name of the storage account"
+  description = "Storage account name"
   type        = string
   default     = "checkovdemostorage"
 }
-
-variable "container_name" {
-  description = "Name of the storage container for tfstate"
-  type        = string
-  default     = "tfstate"
-}
-
